6f7eec5a1cc7b41a24ac6e2453cfc10bb85d0d8e
prajwalpatil392 <your-email@example.com>
2026-03-04 17:15:18 +0530

    Update code

---
+++ push-to-github.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Pushing to GitHub...
+echo.
+git add .
+git commit -m "Update code"
+git push https://prajwalpatil392@github.com/prajwalpatil392/Mahalakshmi-Imitation-Jewellery.git main
+echo.
+echo Done!
+pause
